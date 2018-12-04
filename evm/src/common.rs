71473e85ed97f47666d61dd87226b4540c623d46
mohanson <mohanson@outlook.com>
2018-12-04 10:21:23 +0800

    Small changes

--- evm/src/common.rs
+++ evm/src/common.rs
@@ -20,9 +20,8 @@ pub fn mem_gas_cost(size: u64, memory_gas: u64) -> u64 {
 // Get the signed 256 from Unsigned 256.
 #[inline]
 pub fn get_sign(value: U256) -> (U256, bool) {
-    let U256(arr) = value;
     // The highest bit is 1, indicating that it is negative
-    let sign = arr[3] >> 63 == 1;
+    let sign = (value >> 255) == U256::one();
     (set_sign(value, sign), sign)
 }
 
