ef156b1404f4f6d41ba4ef6612fa6c8f088bc2e7
mohanson <mohanson@outlook.com>
2019-02-15 11:16:00 +0800

    Add LRU cache for database

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,3 @@
-use cita_trie::trie::Trie;
-use cita_trie::db::DB;
-use parity_rocksdb::rocksdb::Writable;
-
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum DBError {
     Str(String),
@@ -26,6 +22,7 @@ impl std::error::Error for DBError {}
 
 struct RocksDB {
     raw: parity_rocksdb::DB,
+    lru: lru_cache::LruCache<Vec<u8>, Vec<u8>>,
 }
 
 impl RocksDB {
@@ -33,6 +30,7 @@ impl RocksDB {
         let db = parity_rocksdb::DB::open_default(path)?;
         return Ok(RocksDB {
             raw: db,
+            lru: lru_cache::LruCache::new(65536),
         })
     }
 }
@@ -43,31 +41,39 @@ impl std::fmt::Debug for RocksDB {
     }
 }
 
-// TODO: Global cache?
 impl cita_trie::db::DB for RocksDB {
     type Error = DBError;
 
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
+    fn get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
+        if let Some(data) = self.lru.get_mut(&key.to_vec()) {
+            return Ok(Some(data.to_vec()))
+        }
+
         let a = self.raw.get(key)?;
-        let b = match a {
-            Some(data) => Some(data.to_vec()),
-            None => None,
+        match a {
+            Some(data) => {
+                self.lru.insert(key.to_vec(), data.to_vec());
+                return Ok(Some(data.to_vec()))
+            },
+            None => {
+                return Ok(None)
+            },
         };
-        Ok(b)
     }
 
     fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<(), DBError> {
         self.raw.put(key, value)?;
+        self.lru.insert(key.to_vec(), value.to_vec());
         Ok(())
     }
 
-    fn contains(&self, key: &[u8]) -> Result<bool, DBError> {
-        let r =self.raw.get(key)?;
-        Ok(r.is_some())
+    fn contains(&mut self, key: &[u8]) -> Result<bool, DBError> {
+        Ok(self.get(key)?.is_some())
     }
 
     fn remove(&mut self, key: &[u8]) -> Result<(), DBError> {
         self.raw.delete(key)?;
+        self.lru.remove(&key.to_vec());
         Ok(())
     }
 }
