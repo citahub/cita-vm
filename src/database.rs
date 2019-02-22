e330f5cad9ea2be0b33dd02f8ee5a40f54093605
mohanson <mohanson@outlook.com>
2019-02-22 11:15:38 +0800

    Revert "Lru cache wraps by RefCell now"
    This reverts commit 8f76ed5a3a56a5b791abf2aaefc6c01b16b61d80.

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use parity_rocksdb::rocksdb::Writable;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum DBError {
+enum DBError {
     Str(String),
 }
 
@@ -15,23 +15,24 @@ impl std::fmt::Display for DBError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             DBError::Str(data) => write!(f, "{:?}", data),
+            _ => unimplemented!(),
         }
     }
 }
 
 impl std::error::Error for DBError {}
 
-pub struct RocksDB {
+struct RocksDB {
     raw: parity_rocksdb::DB,
-    lru: std::sync::Mutex<std::cell::RefCell<lru_cache::LruCache<Vec<u8>, Vec<u8>>>>,
+    lru: lru_cache::LruCache<Vec<u8>, Vec<u8>>,
 }
 
 impl RocksDB {
-    pub fn new(path: &str) -> Result<Self, DBError> {
+    fn new(path: &str) -> Result<Self, DBError> {
         let db = parity_rocksdb::DB::open_default(path)?;
         return Ok(RocksDB {
             raw: db,
-            lru: std::sync::Mutex::new(std::cell::RefCell::new(lru_cache::LruCache::new(65536))),
+            lru: lru_cache::LruCache::new(65536),
         });
     }
 }
@@ -45,16 +46,15 @@ impl std::fmt::Debug for RocksDB {
 impl cita_trie::db::DB for RocksDB {
     type Error = DBError;
 
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
-        let cache = self.lru.lock().unwrap();
-        if let Some(data) = cache.borrow_mut().get_mut(&key.to_vec()) {
+    fn get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
+        if let Some(data) = self.lru.get_mut(&key.to_vec()) {
             return Ok(Some(data.to_vec()));
         }
 
         let a = self.raw.get(key)?;
         match a {
             Some(data) => {
-                cache.borrow_mut().insert(key.to_vec(), data.to_vec());
+                self.lru.insert(key.to_vec(), data.to_vec());
                 return Ok(Some(data.to_vec()));
             }
             None => return Ok(None),
@@ -62,20 +62,18 @@ impl cita_trie::db::DB for RocksDB {
     }
 
     fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<(), DBError> {
-        let cache = self.lru.lock().unwrap();
         self.raw.put(key, value)?;
-        cache.borrow_mut().insert(key.to_vec(), value.to_vec());
+        self.lru.insert(key.to_vec(), value.to_vec());
         Ok(())
     }
 
-    fn contains(&self, key: &[u8]) -> Result<bool, DBError> {
+    fn contains(&mut self, key: &[u8]) -> Result<bool, DBError> {
         Ok(self.get(key)?.is_some())
     }
 
     fn remove(&mut self, key: &[u8]) -> Result<(), DBError> {
-        let cache = self.lru.lock().unwrap();
         self.raw.delete(key)?;
-        cache.borrow_mut().remove(&key.to_vec());
+        self.lru.remove(&key.to_vec());
         Ok(())
     }
 }
